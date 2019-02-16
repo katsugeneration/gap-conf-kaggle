2e7570fafd42620e1ee51f1bfe75de633b69ed1f
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2019-02-16 16:19:53 +0900

    Add vectorize pos list function

--- models/stanfordnlp_model.py
+++ models/stanfordnlp_model.py
@@ -1,13 +1,28 @@
 import pandas
 import utils
 from collections import namedtuple
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
 
 
+dtype = np.int32
 START_OF_SENTENCE = "SOS"
 END_OF_SENTENCE = "EOS"
+POS_TYPES = ["CC", "CD", "DT", "EX", "FW", "IN", "JJ", "JJR", "JJS", "LS", "MD", "NN", "NNS", "NNP", "NNPS", "PDT", "POS", "PRP", "PRP$", "RB", "RBR", "RBS", "RP", "SYM", "TO", "UH", "VB", "VBD", "VBG", "VBN", "VBP", "VBZ", "WDT", "WP", "WP$", "WRB"]
+cv = CountVectorizer(dtype=dtype)
+cv.fit(POS_TYPES + [START_OF_SENTENCE, END_OF_SENTENCE])
+print(cv.vocabulary_)
 
 
 def _get_bag_of_pos(words, index, N):
+    """Return pos list surrounding index
+    Args:
+        words (list): stanfordnlp word list object having pos attributes.
+        index (int): target index
+        N (int): return +/- N word pos
+    Return:
+        pos_list (List[str]): xpo format string list
+    """
     DummyWord = namedtuple("DummyWord", "pos")
     sos = DummyWord(pos=START_OF_SENTENCE)
     eos = DummyWord(pos=END_OF_SENTENCE)
@@ -16,6 +31,24 @@ def _get_bag_of_pos(words, index, N):
     return [w.pos for w in words[index-N:index+N+1]]
 
 
+def _vectorise_bag_of_pos(words, indexes, N):
+    """Return pos list surrounding index
+    Args:
+        words (list): stanfordnlp word list object having pos attributes.
+        indexes (List[int]): target indexes
+        N (int): return +/- N word pos
+    Return:
+        pos_list (List[str]): xpo format string list
+    """
+    matrixes = []
+    for index in indexes:
+        poss = _get_bag_of_pos(words, index, N)
+        matrixes.append(" ".join(poss))
+    return cv.transform(matrixes)
+
+
 def evaluate(test_data):
     df = pandas.read_csv('dataset/gap-development.tsv', sep='\t')
-    utils.charpos_to_word_index(df['Text'], df['Pronoun-offset'])
+
+    for i in range(10):
+        words, index = utils.charpos_to_word_index(df.iloc(i)['Text'], df.iloc(i)['Pronoun-offset'])
