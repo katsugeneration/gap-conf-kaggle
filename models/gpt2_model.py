7bb3b43862cfccdd9a26526f430f8d2b0920b815
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2019-03-09 16:31:16 +0900

    Add gpt-2 model

---
+++ models/gpt2_model.py
@@ -0,0 +1,57 @@
+# Limitations under the MIT License.
+# Copyright 2019 Katsuya Shimabukuro.
+
+import os
+import json
+import numpy as np
+import tensorflow as tf
+import importlib.util
+
+# Load Open AI GPT-2 module
+gpt2_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "gpt-2")
+spec = importlib.util.spec_from_file_location("model", os.path.join(gpt2_path, "src", "model.py"))
+model = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(model)
+spec = importlib.util.spec_from_file_location("encoder", os.path.join(gpt2_path, "src", "encoder.py"))
+encoder = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(encoder)
+
+model_name = '117M'
+sess = None
+enc = encoder.get_encoder(model_name)
+targets = None
+context = None
+
+
+def build():
+    global sess
+    global targets
+    global context
+
+    hparams = model.default_hparams()
+    with open(os.path.join(gpt2_path, 'models', model_name, 'hparams.json')) as f:
+        hparams.override_from_dict(json.load(f))
+
+    context = tf.placeholder(tf.int32, [1, None])
+    lm_output = model.model(hparams=hparams, X=context, past=None, reuse=tf.AUTO_REUSE)
+    logits = lm_output['logits'][:, :, :hparams.n_vocab]
+    values, indices = tf.nn.top_k(logits[:, -1, :], k=10)
+    indices = tf.reshape(indices, [10, 1])
+
+    sess = tf.Session()
+    saver = tf.train.Saver()
+    ckpt = tf.train.latest_checkpoint(os.path.join(gpt2_path, 'models', model_name))
+    saver.restore(sess, ckpt)
+
+    targets = indices
+    context = context
+
+
+def predict(text):
+    context_tokens = enc.encode("Phoebe Thomas played Cheryl Cassidy, Pauline's friend and also a year 11 pupil in Simon's class. Dumped her boyfriend following Simon's advice after he wouldn't have sex with her but later realised this was due to him catching crabs off her friend Pauline.\nQ: What's her name?\nA:")
+    out = sess.run(targets, {context: [context_tokens]})
+    return [enc.decode(o).strip() for o in out]
+
+
+def evaluate(test_data):
+    predict()
