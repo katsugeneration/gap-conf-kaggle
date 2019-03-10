68b275a9aaacf599a61a3efabc3755a27fb02213
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2019-03-10 19:04:57 +0900

    Divide gpt-2 estimate model

---
+++ gpt2_estimator.py
@@ -0,0 +1,89 @@
+# Limitations under the MIT License.
+# Copyright 2019 Katsuya Shimabukuro.
+
+import os
+import re
+import json
+import tensorflow as tf
+import importlib.util
+
+# Load Open AI GPT-2 module
+gpt2_path = os.path.join(os.path.dirname(__file__), "gpt-2")
+spec = importlib.util.spec_from_file_location("model", os.path.join(gpt2_path, "src", "model.py"))
+model = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(model)
+spec = importlib.util.spec_from_file_location("encoder", os.path.join(gpt2_path, "src", "encoder.py"))
+encoder = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(encoder)
+
+model_name = '117M'
+sess = None
+enc = encoder.get_encoder(model_name)
+targets = None
+context = None
+rates = None
+
+
+def build():
+    """Build model."""
+    global sess
+    global targets
+    global rates
+    global context
+
+    hparams = model.default_hparams()
+    with open(os.path.join(gpt2_path, 'models', model_name, 'hparams.json')) as f:
+        hparams.override_from_dict(json.load(f))
+
+    context = tf.placeholder(tf.int32, [1, None])
+    lm_output = model.model(hparams=hparams, X=context, past=None, reuse=tf.AUTO_REUSE)
+    logits = lm_output['logits'][:, :, :hparams.n_vocab]
+    values, indices = tf.nn.top_k(logits[:, -1, :], k=5)
+    rates = tf.math.softmax(values)
+    indices = tf.reshape(indices, [5, 1])
+
+    sess = tf.Session()
+    saver = tf.train.Saver()
+    ckpt = tf.train.latest_checkpoint(os.path.join(gpt2_path, 'models', model_name))
+    saver.restore(sess, ckpt)
+
+    targets = indices
+    rates = rates
+    context = context
+
+
+def predict(text):
+    """Predict next word.
+
+    Args:
+        text (str): input text for predction
+    Return:
+        predictions (List[Tuple[str, int]]): preidcted word and probability list.
+    """
+    context_tokens = enc.encode(text)
+    words, probabilities = sess.run([targets, rates], {context: [context_tokens]})
+    return [(enc.decode(o).strip(), p) for o, p in zip(words, probabilities[0])]
+
+
+def _get_scope_sentence(words, indexes):
+    """Return target sentence
+
+    Args:
+        words (List[Words]): stanfordnlp word object list.
+        indexe (List[int]): target index list. format is [Pronoun, A, B]
+    Return:
+        sentence (str): target word contains sentence.
+    """
+    roots = [(i, w) for i, w in enumerate(words) if int(w.index) == 1]
+    start_index = 0
+    end_index = len(words) - 1
+    for i, w in roots:
+        if (i <= indexes).all():
+            start_index = i
+        elif (i >= indexes).all():
+            end_index = i - 1
+            break
+    sentence = " ".join([w.text for w in words[start_index:end_index+1]])
+    sentence = re.sub(r" (\W)", r"\1", sentence)
+    sentence = re.sub(r" n't", r"n't", sentence)
+    return sentence
--- models/gpt2_model.py
+++ models/gpt2_model.py
@@ -1,102 +1,17 @@
 # Limitations under the MIT License.
 # Copyright 2019 Katsuya Shimabukuro.
 
-import os
-import re
-import json
+import gpt2_estimator
 import pandas
 import numpy as np
-import tensorflow as tf
-import importlib.util
 from collections import namedtuple
 from sklearn.metrics import accuracy_score, log_loss, confusion_matrix
 from models import stanfordnlp_model
 
-# Load Open AI GPT-2 module
-gpt2_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "gpt-2")
-spec = importlib.util.spec_from_file_location("model", os.path.join(gpt2_path, "src", "model.py"))
-model = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(model)
-spec = importlib.util.spec_from_file_location("encoder", os.path.join(gpt2_path, "src", "encoder.py"))
-encoder = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(encoder)
-
-model_name = '117M'
-sess = None
-enc = encoder.get_encoder(model_name)
-targets = None
-context = None
-rates = None
-
 DummyWord = namedtuple("DummyWord", "upos dependency_relation text")
 
 
-def build():
-    """Build model."""
-    global sess
-    global targets
-    global rates
-    global context
-
-    hparams = model.default_hparams()
-    with open(os.path.join(gpt2_path, 'models', model_name, 'hparams.json')) as f:
-        hparams.override_from_dict(json.load(f))
-
-    context = tf.placeholder(tf.int32, [1, None])
-    lm_output = model.model(hparams=hparams, X=context, past=None, reuse=tf.AUTO_REUSE)
-    logits = lm_output['logits'][:, :, :hparams.n_vocab]
-    values, indices = tf.nn.top_k(logits[:, -1, :], k=5)
-    rates = tf.math.softmax(values)
-    indices = tf.reshape(indices, [5, 1])
-
-    sess = tf.Session()
-    saver = tf.train.Saver()
-    ckpt = tf.train.latest_checkpoint(os.path.join(gpt2_path, 'models', model_name))
-    saver.restore(sess, ckpt)
-
-    targets = indices
-    rates = rates
-    context = context
-
-
-def predict(text):
-    """Predict next word.
-
-    Args:
-        text (str): input text for predction
-    Return:
-        predictions (List[Tuple[str, int]]): preidcted word and probability list.
-    """
-    context_tokens = enc.encode(text)
-    words, probabilities = sess.run([targets, rates], {context: [context_tokens]})
-    return [(enc.decode(o).strip(), p) for o, p in zip(words, probabilities[0])]
-
-
-def _get_scope_sentence(words, indexes):
-    """Return target sentence
-
-    Args:
-        words (List[Words]): stanfordnlp word object list.
-        indexe (List[int]): target index list. format is [Pronoun, A, B]
-    Return:
-        sentence (str): target word contains sentence.
-    """
-    roots = [(i, w) for i, w in enumerate(words) if int(w.index) == 1]
-    start_index = 0
-    end_index = len(words) - 1
-    for i, w in roots:
-        if (i <= indexes).all():
-            start_index = i
-        elif (i >= indexes).all():
-            end_index = i - 1
-            break
-    sentence = " ".join([w.text for w in words[start_index:end_index+1]])
-    sentence = re.sub(r" (\W)", r"\1", sentence)
-    sentence = re.sub(r" n't", r"n't", sentence)
-    return sentence
-
-
-def calcurate_likelihood(words, indexes, y):
+def calcurate_likelihood(words, indexes):
     """Return choice likelihoods
 
     Args:
@@ -106,9 +21,9 @@ def calcurate_likelihood(words, indexes, y):
         rates (List[float]): selection likelihood. [A, B, NEITHER]
     """
     woman = ["she", "her"]
-    sentence = _get_scope_sentence(words, np.array(indexes))
+    sentence = gpt2_estimator._get_scope_sentence(words, np.array(indexes))
     gender = "her" if words[indexes[0]].text.lower() in woman else "his"
-    predicts = predict(sentence + "\nQ: What's " + gender + " name?\nA:")
+    predicts = gpt2_estimator.predict(sentence + "\nQ: What's " + gender + " name?\nA:")
 
     A_rate = 0.00
     B_rate = 0.00
@@ -207,7 +122,7 @@ def calculate_syntax_likelihood(words, indexes):
 
 
 def evaluate(test_data, use_preprocessdata=True):
-    build()
+    gpt2_estimator.build()
     validation_df = pandas.read_csv('dataset/gap-validation.tsv', sep='\t')
     data = stanfordnlp_model._load_data(validation_df, use_preprocessdata, 'preprocess_valdata.pkl')
     Y = stanfordnlp_model._get_classify_labels(validation_df)
